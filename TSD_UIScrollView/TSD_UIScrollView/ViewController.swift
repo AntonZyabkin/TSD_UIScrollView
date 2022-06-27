7886d7afa1684afb028e629eaf6a1e4c35b84a89
AntonZyabkin <zyabkinantonv@gmail.com>
2022-06-27 14:27:49 +0300

    1 commit

---
+++ TSD_UIScrollView/TSD_UIScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TSD_UIScrollView
+//
+//  Created by Anton Zyabkin on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
