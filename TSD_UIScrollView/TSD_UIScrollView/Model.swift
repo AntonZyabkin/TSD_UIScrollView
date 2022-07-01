222d18952168aacca5257f02289509214f563a16
AntonZyabkin <zyabkinantonv@gmail.com>
2022-07-01 12:40:13 +0300

    5 commit

--- TSD_UIScrollView/TSD_UIScrollView/Model.swift
+++ TSD_UIScrollView/TSD_UIScrollView/Model.swift
@@ -9,15 +9,37 @@ import UIKit
 
 
 let curentItem = 0
-
+var itemScrollView = UIScrollView ()
+var widthOfItemScrollView = 15
 let albosItemArray = ["iphone1", "iwatch1", "case1"]
 let titleItemImage = ["Смартфон Apple iPhone 11 64GB, Белый", "Металлический ремешок на руку для смарт часов Apple Watch series 1-6 и Эппл Вотч SE 42-44 mm" , "Чехол силиконовый на айфон 11 с защитой для камеры, прозрачный"]
 
 
-func createBlock() {
+func createBlock (vC: UIViewController) {
+    var underView = UIView ()
     
+    for i in 0..<albosItemArray.count {
+
+        widthOfItemScrollView += 170
+        underView = UIView (frame: CGRect (x: (15 + i * 170), y: 0, width: 155, height: 200))
+        underView.backgroundColor = #colorLiteral(red: 0.09435228258, green: 0.08189914376, blue: 0.1025743857, alpha: 1)
+        underView.layer.cornerRadius = 20
+        underView.layer.masksToBounds = true
+        
+        itemScrollView.addSubview(underView)
+    }
+    itemScrollView.contentSize = CGSize (width: widthOfItemScrollView, height: 200)
 }
 
 func  createUnderView () {
     
+}
+
+//настроим скроллВью для визуализации айтемов
+func generateItemScrollView (vC: UIViewController) {
+            
+            itemScrollView = UIScrollView (frame: CGRect (x: 0, y: 240, width: vC.view.frame.width, height: 200))
+    itemScrollView.backgroundColor = .white
+    itemScrollView.contentSize = CGSize (width: 600, height: 200)
+            vC.view.addSubview(itemScrollView)
 }
--- TSD_UIScrollView/TSD_UIScrollView/SearchViewController.swift
+++ TSD_UIScrollView/TSD_UIScrollView/SearchViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 class SearchViewController: UIViewController {
 
     var imageView = UIImageView ()
-    var itemScrollView = UIScrollView ()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        generateItemScrollView ()
+        generateItemScrollView (vC: self)
+        createBlock (vC: self)
+        
     }
 
     
-    
-    //настроим скроллВью для визуализации айтемов
-    func generateItemScrollView () {
-        itemScrollView = UIScrollView (frame: CGRect (x: 0, y: 240, width: view.frame.width, height: 200))
-        itemScrollView.backgroundColor = .white
-        itemScrollView.contentSize = CGSize (width: 600, height: 200)
-        view.addSubview(itemScrollView)
-    }
+
 }
